9f55f7662f2ac680b4e8ac0b0e12b7ac7122f3ed
MrBennySolomon <MrBennySolomon@gmail.com>
2023-01-14 03:22:28 +0200

    03-Amazon-Assignment ver 3.5

--- js/script.js
+++ js/script.js
@@ -1,33 +1,43 @@
-const img1 = document.getElementById("small-1");
-const img2 = document.getElementById("small-2");
+// const img1 = document.getElementById("small-1");
+// const img2 = document.getElementById("small-2");
 
-const thumbnails = [img1, img2];
-const focusImages = ["/img/main/main-left-small-1.jpg", "/img/main/main-left-small-2.jpg"];
+// const thumbnails = [img1, img2];
+// const focusImages = ["/img/main/main-left-small-1.jpg", "/img/main/main-left-small-2.jpg"];
 
-const focusImg = document.getElementById("big");
 
-const activateGallery = () => {
-  thumbnails.forEach((thumbnail, i) => {
-    thumbnail.addEventListener("mouseover", function () {
-      focusImg.style.background = `url('${focusImages[i]}') no-repeat center center/contain`;
-    });
-  });
+// const onMouseOver = () => {
+//   const focusImg = document.getElementById("big");
+
+//   document.getElementById("small-1").addEventListener('mouseover', () => 
+//   focusImg.style.background = "url('/img/main/main-left-small-1.jpg') no repeat center center/cover");
+
+//   document.getElementById("small-2").addEventListener('mouseover', () => 
+//   focusImg.style.background = "url('/img/main/main-left-small-2.jpg') no repeat center center/cover");
+// }
+
+
+// const activateGallery = () => {
+//   thumbnails.forEach((thumbnail, i) => {
+//     thumbnail.addEventListener("mouseover", function () {
+//       focusImg.style.background = `url('${focusImages[i]}') no-repeat center center/contain`;
+//     });
+//   });
   
-  thumbnails.forEach((thumbnail, i) => {
-    thumbnail.addEventListener("click", function () {
-      focusImg.style.background = `url('${focusImages[i]}') no-repeat center center/contain`;
-    });
-  });
+//   thumbnails.forEach((thumbnail, i) => {
+//     thumbnail.addEventListener("click", function () {
+//       focusImg.style.background = `url('${focusImages[i]}') no-repeat center center/contain`;
+//     });
+//   });
 
-  thumbnails.forEach((thumbnail, i) => {
-    thumbnail.addEventListener("mouseleave", function () {
-      focusImg.style.background = `url('${focusImages[i]}') no-repeat center center/contain`;
-    });
-  });
-};
+//   thumbnails.forEach((thumbnail, i) => {
+//     thumbnail.addEventListener("mouseleave", function () {
+//       focusImg.style.background = `url('${focusImages[i]}') no-repeat center center/contain`;
+//     });
+//   });
+// };
 
-const init = () => {
-  activateGallery();
-};
+// const init = () => {
+//   activateGallery();
+// };
 
-init();
+// init();
